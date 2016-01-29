f9ad687d39fd9b8dfd8d5223747d4aa15f2b6080
Bart Vaerenberg <vaerenberg@gmail.com>
2016-01-29 10:41:16 +0100

    redirect /research to ResearchGate

---
+++ src/vaerenberg.com/Controllers/ResearchController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Vaerenberg.Controllers
+{
+    [Route("[controller]")]
+    public class ResearchController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Redirect("https://www.researchgate.net/profile/Bart_Vaerenberg");
+        }
+    }
+}
